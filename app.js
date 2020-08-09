d90e7d5c672db1cb244826c7d6834cc7cee76883
Manav <manav10799@gmail.com>
2020-08-09 13:52:06 +0530

    Add gitignore Procfile and update

--- app.js
+++ app.js
@@ -126,6 +126,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 app.listen(port);
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
